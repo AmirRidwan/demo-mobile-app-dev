905aedb1a23f50769c1359b4155c8dcc81520e16
siowrs <rs.sioww@gmail.com>
2025-03-18 02:31:18 +0800

    booking summary wip

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
@@ -28,8 +29,15 @@ class TicketController extends Controller
         // then only set seat to booked after completing payment
     }
 
-    public function bookingSummary(Request $request)
+    public function bookingSummary($id, Request $request)
     {
+
+        // fetch movie details
+        $movie = Http::withHeaders([
+            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWFkM2EwMTIwYTAzYzRjZWQ3NzllZWY0M2I4Y2I2YSIsIm5iZiI6MTc0MTk2MDQwNC43NjQsInN1YiI6IjY3ZDQzNGQ0OTE0ZGU4ZDg5MTAyOTNlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6ZOdBTA-t0s0K9NWINQ7qBfd9Sj8CUi4pT0JhaQYF4g',
+            'accept' => 'application/json',
+        ])->get('https://api.themoviedb.org/3/movie/' . $id . '?language=en-US')->json();
+
 
         $selected_seats = $request->all();
 
@@ -46,9 +54,14 @@ class TicketController extends Controller
         $new_seats = json_encode($seats);
 
         // save the new file
+        // somehow doing this will cause the page to refresh
+        // so any operation must be placed before this line
         Storage::disk('public')->put('seat-status.json', $new_seats);
 
-        return Inertia::render('booking-summary');
+
+        return Inertia::render('booking-summary', [
+            'details' => $movie
+        ]);
         // return $request->all();
     }
 }
--- resources/js/pages/booking-summary.tsx
+++ resources/js/pages/booking-summary.tsx
@@ -1,13 +1,67 @@
 import { AppHeader } from '@/components/app-header';
+import MovieImage from '@/components/movie-image';
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
 import { AppBottomNavLayout } from '@/layouts/app-bottom-nav-layout';
 import AppLayout from '@/layouts/app-layout';
+import { MovieType } from './welcome';
 
-export default function Movie({}: {}) {
+export default function Movie({ details }: { details: MovieType }) {
     return (
         <>
-            <AppHeader backBtn={true} title="Ticket Booking" />
-            <AppLayout>'aaa'</AppLayout>
+            <AppHeader backBtn={true} title="Booking Summary" />
+            <AppLayout>
+                <div className="space-y-3 border p-3">
+                    <div className="flex gap-4 py-1">
+                        <div className="w-32">
+                            <MovieImage src={details.poster_path} />
+                        </div>
+                        <div className="space-y-3">
+                            <div className="space-y-1">
+                                <h4 className="text-lg font-bold">{details.original_title}</h4>
+                                <div className="space-x-2">
+                                    {details.genres.map((g) => (
+                                        <Badge key={g.id} variant="outline">
+                                            {g.name}
+                                        </Badge>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="space-y-1">
+                                <p className="space-x-0.5 text-xs">
+                                    <span>
+                                        {Math.floor(details.runtime / 60)} hour
+                                        {Math.floor(details.runtime / 60) > 1 && 's'}
+                                    </span>
+                                    <span>
+                                        {details.runtime % 60} minute
+                                        {Math.floor(details.runtime % 60) > 1 && 's'}
+                                    </span>
+                                </p>
+
+                                <p className="text-xs">English, IMAXX 3D</p>
+                                <p className="text-xs">Classic tickets</p>
+                            </div>
+                        </div>
+                    </div>
+                    <Separator />
+                    <div>
+                        <Label className="text-xs text-zinc-500">Cinema</Label>
+                        <p className="text-sm">Sunway Gurney</p>
+                    </div>
+                    <div>
+                        <Label className="text-xs text-zinc-500">Cinema</Label>
+                        <p className="text-sm">Sunway Gurney</p>
+                    </div>
+
+                    <div>
+                        <Label className="text-xs text-zinc-500">Cinema</Label>
+                        <p className="text-sm">Sunway Gurney</p>
+                    </div>
+                </div>
+            </AppLayout>
             <AppBottomNavLayout className="p-3">
                 <Button asChild className="w-full">
                     {/* <Link href={route('book.summary')}>Book Ticket</Link> */}
